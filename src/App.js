ae4bafd66de3bc783ee7ea17f2c0421ad4bea34a
SimonImanigirimpuhwe <softimag08@gmail.com>
2021-01-12 20:18:59 +0200

    chore(setup): setting up react project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Welcome to Lost and found web app</h1>
+    </div>
+  );
+}
+
+export default App;
