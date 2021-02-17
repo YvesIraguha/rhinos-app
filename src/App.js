f5405c968db5f9e88b1589cf9e7210a8e3818a35
YvesIraguha <iraguhaivos@gmail.com>
2021-02-17 12:52:10 +0200

    add svgs for login page layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { store } from "./redux";
-import LoginForm from "./components/LoginForm";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
-    <div>
+    <div style={{ backgroundColor: " #0891BF " }}>
       <h1 className="text-center mt-28 font-mono text-4xl text-blue-500 font-bold transform hover:scale-50 transition ease-in duration-500 cursor-pointer tracking-wider">
         🤗 Welcome to Lost and found web app!
       </h1>
-      <LoginForm />
+      <Login />
     </div>
   );
 };
